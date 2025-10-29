5555330d832e559f259f322e446e19402ad91ed7
jimee222 <jarayare@ucenfotec.ac.cr>
2025-10-29 14:00:36 -0600

    Update UserRepository.java (existsByEmail + query firstName)

--- src/main/java/com/muse_ai/logic/entity/user/UserRepository.java
+++ src/main/java/com/muse_ai/logic/entity/user/UserRepository.java
@@ -2,18 +2,20 @@ package com.muse_ai.logic.entity.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long>  {
-    @Query("SELECT u FROM User u WHERE LOWER(u.name) LIKE %?1%")
-    List<User> findUsersWithCharacterInName(String character);
-
-    @Query("SELECT u FROM User u WHERE u.name = ?1")
-    Optional<User> findByName(String name);
-
-    Optional<User> findByLastname(String lastname);
+    @Query("""
+         SELECT u
+         FROM User u
+         WHERE LOWER(u.firstName) LIKE LOWER(CONCAT('%', :term, '%'))
+         """)
+    List<User> findUsersWithCharacterInName(@Param("term") String term);
 
     Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
