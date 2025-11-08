85e9522d45f4e2070342a5f080596b657ab91983
dcruzca <160550702+dcruzcastro@users.noreply.github.com>
2025-11-07 20:24:55 -0600

    Unit Test
    User Repository Test

---
+++ src/test/java/com/muse_ai/DummyTest.java
@@ -0,0 +1,12 @@
+package com.muse_ai;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DummyTest {
+
+    @Test
+    void basicSanityTest() {
+        assertTrue(true);
+    }
+}
